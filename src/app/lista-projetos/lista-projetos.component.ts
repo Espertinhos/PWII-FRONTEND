8ccdb72394d34e3d87d73b2f54cd591102d26de8
Espertinhos <murillo_1230@hotmail.com>
2019-11-11 22:19:33 -0300

    Front End 3

--- src/app/lista-projetos/lista-projetos.component.ts
+++ src/app/lista-projetos/lista-projetos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProjetoApiService } from '../service/projeto-api.service';
+import { Projeto } from '../model/projeto';
 
 @Component({
   selector: 'has-lista-projetos',
@@ -7,11 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./lista-projetos.component.css']
 })
 export class ListaProjetosComponent implements OnInit {
-projetos = [{"nome":"Projeto 1","sala":"Lab 08","turma": "2Q" },
-            {"nome":"Projeto 2","sala":"Lab 09","turma": "3Q"} ];
-  constructor(private raiter : Router) { }
+projetos : Projeto[];
+  constructor(private raiter : Router,
+              private service : ProjetoApiService) { }
 
   ngOnInit() {
+      this.service.getProjetos()
+                  .subscribe((dados : Projeto[]) => this.projetos = dados,
+                              error => console.log(error)); 
   }
   detalhes(id : number){
     this.raiter.navigate(['detalheprojeto',id]);
---
+++ src/app/model/projeto.ts
@@ -0,0 +1,9 @@
+export class Projeto {
+    private id:number;
+    private nome:string;
+    private orientadores:string;
+    private sala:string;
+    private turma:string;
+
+
+}
